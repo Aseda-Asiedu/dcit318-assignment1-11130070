331f9549ba7e29bb1959102220cab1cf4ae32695
Aseda Asiedu <asieduaseda10@gmail.com>
2025-08-04 01:54:46 +0000

    Ticket Price Calculator

---
+++ Ticket Price Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicketPriceCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                int price = (age <= 12 || age >= 65) ? 7 : 10;
+                Console.WriteLine($"Your ticket price is: GHC{price}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+            }
+        }
+    }
+}
